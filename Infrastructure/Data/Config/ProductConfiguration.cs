613171e07e253a27b780c0d786fcce644bef1c76
Carlos Ramirez <c.and.95@outlook.es>
2020-06-25 19:42:02 -0700

    Added migrations and Config metadata

---
+++ Infrastructure/Data/Config/ProductConfiguration.cs
@@ -0,0 +1,22 @@
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Config
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(property => property.Id).IsRequired();
+            builder.Property(property => property.Name).IsRequired().HasMaxLength(100);
+            builder.Property(property => property.Description).IsRequired().HasMaxLength(180);
+            builder.Property(property => property.Price).HasColumnType("decimal(18,2)");
+            builder.Property(property => property.PictureUrl).IsRequired();
+            builder.HasOne(brand => brand.ProductBrand).WithMany()
+            .HasForeignKey(brand => brand.ProductBrandId);
+            builder.HasOne(type => type.ProductType).WithMany()
+            .HasForeignKey(type => type.ProductTypeId); 
+        }
+    }
+}
